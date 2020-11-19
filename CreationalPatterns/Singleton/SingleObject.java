cee3e2e95715cb958b71ba86b199bbe35d4b737a
Trung Quoc Nguyen <johnnguyen2202@outlook.com>
2020-11-20 00:17:18 +0700

    Add Singleton Pattern

---
+++ CreationalPatterns/Singleton/SingleObject.java
@@ -0,0 +1,22 @@
+package CreationalPatterns.Singleton;
+
+public class SingleObject {
+  // Create an object of SingleObject
+  private static SingleObject instance;
+
+  // Make the constructor private so that this class cannot be
+  // instantiated
+  private SingleObject() {};
+
+  // Get the only object available
+  public static SingleObject getInstance() {
+    if (instance == null) {
+      instance = new SingleObject();     
+    }
+    return instance;
+  }
+
+  public void showMessage() {
+    System.out.println("Hello World");
+  }
+}
